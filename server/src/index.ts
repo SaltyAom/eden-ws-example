a462ee3142d0a7729d644cd7f5b9c4bb00a4d702
saltyaom <saltyaom@gmail.com>
2023-01-04 19:52:21 +0700

    :tada: feat: deinit submodule

---
+++ server/src/index.ts
@@ -0,0 +1,65 @@
+import { Elysia, t } from 'elysia'
+import { websocket } from '@elysiajs/websocket'
+import { cors } from '@elysiajs/cors'
+import { staticPlugin } from '@elysiajs/static'
+
+const app = new Elysia()
+    .use(cors())
+    .use(websocket())
+    .use(
+        staticPlugin({
+            path: 'assets',
+            prefix: '/assets'
+        })
+    )
+    .get('/', ({ set }) => {
+        set.redirect = '/assets/index.html'
+    })
+    .ws('/chat', {
+        open(ws) {
+            const { room, name } = ws.data.query
+
+            ws.subscribe(room).publish(room, {
+                message: `${name} has enter the room`,
+                name: 'notice',
+                time: Date.now()
+            })
+        },
+        message(ws, message) {
+            const { room, name } = ws.data.query
+
+            ws.publish(room, {
+                message,
+                name,
+                time: Date.now()
+            })
+        },
+        close(ws) {
+            const { room, name } = ws.data.query
+
+            ws.publish(room, {
+                message: `${name} has leave the room`,
+                name: 'notice',
+                time: Date.now()
+            })
+        },
+        schema: {
+            body: t.String(),
+            query: t.Object({
+                room: t.String(),
+                name: t.String()
+            }),
+            response: t.Object({
+                message: t.String(),
+                name: t.String(),
+                time: t.Number()
+            })
+        }
+    })
+    .listen(3000)
+
+export type App = typeof app
+
+console.log(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+)
